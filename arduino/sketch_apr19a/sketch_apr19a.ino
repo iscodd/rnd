75f2841c71447f9ec2eba83a28bafbc18330b5d4
iscodd <jim.llllong@gmail.com>
2023-04-20 12:28:31 -0400

    Revert "Revert "rnd final test 1""
    This reverts commit cf17ab247ca54cb4600d72b8a67079c76d26ac0e.

---
+++ arduino/sketch_apr19a/sketch_apr19a.ino
@@ -0,0 +1,35 @@
+const int buttonPin = 2;  // the pin that the pushbutton is attached to
+
+// Variables will change:
+int buttonState = 0;        // current state of the button
+int lastButtonState = 0;    // previous state of the button
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize the LED as an output:
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      // if the current state is HIGH then the button went from off to on:
+      Serial.println("ON");
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println("OFF");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
+}
